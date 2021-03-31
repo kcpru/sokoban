8f8f8c92dd1f12bd450295feb4a06848e3749e5b
Ether141 <42118307+Ether141@users.noreply.github.com>
2021-03-31 18:31:03 +0200

    Change buttons, add Button3D transition

--- Assets/Scripts/Interface/Button3D.cs
+++ Assets/Scripts/Interface/Button3D.cs
@@ -3,15 +3,39 @@ using UnityEngine.Events;
 
 public class Button3D : MonoBehaviour
 {
+    [Header("Event settings")]
     public bool isClickable = true;
     public UnityEvent<MonoBehaviour> OnClick = new UnityEvent<MonoBehaviour>();
+
+    [Header("Animation settings")]
+    public float transitionSpeed = 10f;
+    public float scaleMultiplier = 1.1f;
 
     private bool isMouseOver = false;
     private bool click = false;
 
-    private void OnMouseEnter() => isMouseOver = true;
+    private Vector3 startScale;
+    private Vector3 targetScale;
 
-    private void OnMouseExit() => isMouseOver = false;
+    private void Start()
+    {
+        startScale = transform.localScale;
+        targetScale = startScale;
+    }
+
+    private void OnMouseEnter()
+    {
+        isMouseOver = true;
+        targetScale = startScale * scaleMultiplier;
+    }
+
+    private void OnMouseExit()
+    {
+        isMouseOver = false;
+        targetScale = startScale;
+    }
+
+    private void Update() => transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * transitionSpeed);
 
     private void OnMouseDown() => click = true && isClickable;
 
